d7e51d9a6f3c7048d486e46d8d28646d78155793
Froakill <97559883+froaquiu@users.noreply.github.com>
2025-01-13 17:10:00 -0300

    Atualizar o 712_Rats.sh

--- 712_Rats.sh
+++ 712_Rats.sh
@@ -27,8 +27,8 @@ function rainbow_text {
     echo ""  # Nova linha no final
 }
 
-# Arte ASCII antiga
-echo -e "°°
+# Nova arte ASCII
+echo -e "
                _                       __
               /   \\                  /      \\
              '      \\              /          \\
@@ -42,26 +42,26 @@ echo -e "°°
                 \`OOOooOOOOOOOOOooooOOOOOO'OOOo
               .OO \"OOOOOOOOOOOOOOOOOOOO\"OOOOOOOo
           __ OOOOOO\`OOOOOOOOOOOOOOOO\"OOOOOOOOOOOOo
-         ___OOOOOOOO_'OOOOOOOOOOO'_OOOOOOOOOOOOOOOO
-           OOOOO^OOOO0\`(____)/'OOOOOOOOOOOOO^OOOOOO
+         ___OOOOOOOO_\"OOOOOOOOOOO\"_OOOOOOOOOOOOOOOO
+           OOOOO^OOOO0\`(____)/\"OOOOOOOOOOOOO^OOOOOO
            OOOOO OO000/00||00\\000000OOOOOOOO OOOOOO
            OOOOO O0000000000000000 ppppoooooOOOOOO
-           \`OOOOO 0000000000000000 QQQQ 'OOOOOOO'
-            o'OOOO 000000000000000oooooOOoooooooO'
-            OOo'OOOO.00000000000000000000OOOOOOOO'
+           \`OOOOO 0000000000000000 QQQQ \"OOOOOOO\"
+            o\"OOOO 000000000000000oooooOOoooooooO'
+            OOo\"OOOO.00000000000000000000OOOOOOOO'
            OOOOOO QQQQ 0000000000000000000OOOOOOO
           OOOOOO00eeee00000000000000000000OOOOOOOO.
          OOOOOOOO000000000000000000000000OOOOOOOOOO
          OOOOOOOOO00000000000000000000000OOOOOOOOOO
          \`OOOOOOOOO000000000000000000000OOOOOOOOOOO
-           'OOOOOOOO0000000000000000000OOOOOOOOOOO'
-             'OOOOOOO00000000000000000OOOOOOOOOO'
-  .ooooOOOOOOOo'OOOOOOO000000000000OOOOOOOOOOO'
-.OOO''''''''''.oOOOOOOOOOOOOOOOOOOOOOOOOOOOOo
-OOO         QQQQO\°'                     \°QQQQ
+           \"OOOOOOOO0000000000000000000OOOOOOOOOOO'
+             \"OOOOOOO00000000000000000OOOOOOOOOO\"
+  .ooooOOOOOOOo\"OOOOOOO000000000000OOOOOOOOOOO\"
+.OOO\"\"\"\"\"\"\"\"\"\".oOOOOOOOOOOOOOOOOOOOOOOOOOOOOo
+OOO         QQQQO\"'                     \`\"QQQQ
 OOO
-\°OOo.
-  \°'OOOOOOOOOOOOoooooooo....
+\`OOo.
+  \`\"OOOOOOOOOOOOoooooooo....
 " | lolcat
 
 # Nova arte ASCII do título
@@ -86,6 +86,7 @@ echo "2. Внедрить троян на iOS" | lolcat
 echo "3. Внедрить троян на другие устройства" | lolcat
 echo -n "Введите номер: " | lolcat
 read OPTION
+
 
 # Validar entrada do usuário
 case $OPTION in
